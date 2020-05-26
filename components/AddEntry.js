cc34e4373b98e7f2ba93688e8d77609688b12ad0
Minh Nguyen <tmnguyen1403@gmail.com>
2020-05-25 23:48:30 -0500

    create component AddEntry

---
+++ components/AddEntry.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+	state = {
+		run: 0,
+		bike: 0,
+		swim: 0,
+		sleep: 0,
+		eat: 0,
+	}
+	increment = (metric) => {
+		const { max, step } = getMetricMetaInfo(metric)
+
+		this.setState( (state) => {
+			const count = state[metric] + step
+
+			return {
+				...state,
+				[metric]: count > max ? max : count
+			}
+		})
+	}
+	decrement = (metric) => {
+		const { step } = getMetricMetaInfo(metric)
+
+		this.setState( (state) => {
+			const count = state[metric] - step
+
+			return {
+				...state,
+				[metric]: count < 0 ? 0 : count
+			}
+		})
+	}
+	slider = (metric, value) => {
+		this.setState({
+			[metric]: value
+		})
+	}
+	render() {
+		return (
+			<View>
+				<Text>Add Entry</Text>
+			</View>
+		)
+	}
+}
